22a6cbbe1d5788337b6198159ca3fd590145b846
nurmatovabdulbosit <boysa1993@gmail.com>
2024-07-13 18:08:08 +0500

    status

--- horijiy/urls.py
+++ horijiy/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path('export/', views.export_to_excel, name='export_to_excel'),
     path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
    # path('logout', auth_views.LogoutView.as_view(), {'next_page': 'login/?next=/'}, name='logout'),
-    path('logout/', auth_views.LogoutView.as_view(next_page='/login/'), name='logout'),
+    path('', auth_views.LogoutView.as_view(next_page='/login/'), name='logout'),
     # Boshqa yo'nalishlar...
 ]
